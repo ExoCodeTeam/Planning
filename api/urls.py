ff45374f17d642b45e9f2263ddba0c7f5c55d9c7
hsbakir <94084650+DEVBakir@users.noreply.github.com>
2024-10-21 19:21:18 +0100

    Add Init Template

--- api/urls.py
+++ api/urls.py
@@ -5,4 +5,6 @@ from api import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('get', views.get, name='get'),
+    path('timetable', views.timetable_view, name='timetable'),
+
 ]
--- api/views.py
+++ api/views.py
@@ -1,21 +1,35 @@
-from django.http import JsonResponse
+import json
 
-from timefoldai.demo_data import generate_demo_data,DemoData
+from django.http import JsonResponse
+from django.shortcuts import render
+
+from timefoldai.demo_data import generate_demo_data, DemoData
 from timefoldai.domain import Timetable
 from timefoldai.solver import solver_manager
 
 data_sets = {}
 
+
 # Create your views here.
 def index(request):
-        timetable = generate_demo_data(demo_data=DemoData(value='SMALL')).model_dump_json()
-        data_sets['ID'] = timetable
-        solver_manager.solve_and_listen('ID', timetable,
-                                        lambda solution: update_timetable('ID', solution))
+    timetable = generate_demo_data(demo_data=DemoData(value='SMALL'))
+    data_sets['ID'] = timetable
+    solver_manager.solve_and_listen('ID', timetable,
+                                    lambda solution: update_timetable('ID', solution))
+
 
 def update_timetable(problem_id: str, timetable: Timetable):
     global data_sets
     data_sets[problem_id] = timetable
 
+
 def get(request):
-    return JsonResponse(data_sets['ID'], safe=False)
+    return JsonResponse(data_sets['ID'].model_dump_json(), safe=False)
+
+
+def timetable_view(request):
+    # Assuming 'data_sets["ID"]' contains your timetable JSON string
+    timetable_data = json.loads(data_sets["ID"].model_dump_json())  # Convert the JSON string to a Python dictionary
+
+
+    return render(request, 'timetable.html', {'timetable': timetable_data})
